b8c43a9edf2ab10be7a75e5b04c725c961887983
zai1208 <63334410+zai1208@users.noreply.github.com>
2020-06-17 10:36:28 +1000

    Update staticube.py

--- Cube/static/staticube.py
+++ Cube/static/staticube.py
@@ -1,12 +1,17 @@
 """
 Notes:
-IMPORTANT: pygame screen must be made with pygame code
+IMPORTANT: pygame screen must be pre-made with pygame code
+e.g: surface = pygame.display.set_mode((100 -> width, 100 ->height))
+then surface must be passed into the screen argument
 1 - the color attribute must be a tuple such as (225, 0, 0) which is red
 2 - the screen attribute  is what pygame surface to display it to 
 """
+''' 
+Note2: by default draws a red cube if color parameter & extras aren't passed in
+'''
 import pygame
 
-def Cube((x, y, z), size, screen, color):
+def Cube((x, y, z), size, screen, color=(225, 0, 0), wireframe=False, outline=True, outline_width=0, outline_col=(225, 225, 225)):
    X,Y = size = screen.get_width(), screen.get_height()
    truex = x + size / 2
    truey = y - size / 2
@@ -44,32 +49,37 @@ def Cube((x, y, z), size, screen, color):
    Pos2[1], 
    Pos[1]
    ]
-   Pos6 = 
-   # first side (the front) 
-   front = pygame.draw.polygon(screen, color, Pos)
+   Pos6 = [
+   Pos[3],
+   Pos2[3],
+   Pos2[2],
+   Pos1[2]
+   ]
+   if wireframe == False
+       # first side (the front) 
+       front = pygame.draw.polygon(screen, color, Pos)
    
-   # Second side (the back) 
+       # Second side (the back) 
    
-   back = pygame.draw.polygon(screen, color, Pos2)
+       back = pygame.draw.polygon(screen, color, Pos2)
    
-   # Third side (the left) 
+       # Third side (the left) 
    
-   left = pygame.draw.polygon(screen, color, Pos3)
+       left = pygame.draw.polygon(screen, color, Pos3)
    
-   # Fourth side (the right) 
+       # Fourth side (the right) 
    
    
-   right = pygame.draw.polygon(screen, color, Pos4)
-   # fifth side (the top) 
+       right = pygame.draw.polygon(screen, color, Pos4)
+       # fifth side (the top) 
    
-   top = pygame.draw.polygon(screeen, color, Pos5)
+       top = pygame.draw.polygon(screeen, color, Pos5)
    
-   # sixth side(the bottom)
-   pygame.draw.polygon(screen, color, Pos6)
-   
-   # outlines drawn after all colors
-   pygame.draw.lines(screen, (225, 225, 225), Pos)
-   pygame.draw.lines(screen, (225, 225, 225), Pos2)
-   pygame.draw.lines(screen, (225, 225, 225), Pos3)
-   pygame.draw.lines(screen, (225, 225, 225), Pos4)
-   pygame.display.flip()
+       # sixth side(the bottom)
+       pygame.draw.polygon(screen, color, Pos6)
+   if outline == True:
+      # outlines drawn if wanted 
+      pygame.draw.lines(screen, (225, 225, 225), Pos, width=outline_width)
+      pygame.draw.lines(screen, (225, 225, 225), Pos2, width=outline_width)
+      pygame.draw.lines(screen, (225, 225, 225), Pos3, width=outline_width)
+      pygame.draw.lines(screen, (225, 225, 225), Pos4, width=outline_width)
